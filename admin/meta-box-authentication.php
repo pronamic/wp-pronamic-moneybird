590cacdb6a21f2a4e62281982338718716299031
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-25 12:26:44 +0100

    First API response test.

--- admin/meta-box-authentication.php
+++ admin/meta-box-authentication.php
@@ -8,20 +8,41 @@
  * @package   Pronamic\Orbis\Tasks
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+namespace Pronamic\Moneybird;
+
+use Pronamic\WordPress\Http\Facades\Http;
+
+if ( ! \defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$api_token = get_post_meta( $post->ID, '_pronamic_moneybird_api_token', true );
+$api_token = \get_post_meta( $post->ID, '_pronamic_moneybird_api_token', true );
 
 ?>
 <table class="form-table">
 	<tr valign="top">
 		<th scope="row">
-			<label for="pronamic_moneybird_api_token"><?php esc_html_e( 'API token', 'orbis-tasks' ); ?></label>
+			<label for="pronamic_moneybird_api_token"><?php \esc_html_e( 'API token', 'orbis-tasks' ); ?></label>
 		</th>
 		<td>
-			<code><?php echo esc_html( $api_token ); ?></code>
+			<code><?php echo \esc_html( $api_token ); ?></code>
 		</td>
 	</tr>
 </table>
+
+<?php
+
+$response = Http::get(
+	'https://moneybird.com/api/v2/administrations.json',
+	[
+		'headers' => [
+			'Authorization' => 'Bearer ' . $api_token,
+		],
+	]
+);
+
+$data = $response->json();
+
+echo '<pre>';
+var_dump( $data );
+echo '</pre>';
