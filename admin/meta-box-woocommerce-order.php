85bbd70e699b36da470e21209036e869174dfc43
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-04-09 16:59:52 +0200

    Create meta-box-woocommerce-order.php

---
+++ admin/meta-box-woocommerce-order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Meta box WooCommercer order
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2024 Pronamic
+ * @license   GPL-2.0-or-later
+ * @package   Pronamic\Moneybird
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+🧾
